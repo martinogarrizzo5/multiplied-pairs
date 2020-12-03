eabcea0075be1d424e8f5d80788b3854d0cbc0e4
Martin <martinmeneghetti@outlook.it>
2020-12-03 12:32:42 +0100

    Updates

---
+++ precache-manifest.2dedab409bb63179a025102da425fc7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985a8d23328d1133c5bea08742809e0b",
+    "url": "/multiplied-pairs/index.html"
+  },
+  {
+    "revision": "c30570d48c8f5038c1f0",
+    "url": "/multiplied-pairs/static/css/main.4f6026e0.chunk.css"
+  },
+  {
+    "revision": "ee20e709af87075ffbc1",
+    "url": "/multiplied-pairs/static/js/2.5b2fb6b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multiplied-pairs/static/js/2.5b2fb6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30570d48c8f5038c1f0",
+    "url": "/multiplied-pairs/static/js/main.f29f6732.chunk.js"
+  },
+  {
+    "revision": "cd6df493d3468a648815",
+    "url": "/multiplied-pairs/static/js/runtime-main.2983e8ce.js"
+  },
+  {
+    "revision": "f01760eb59116b0f688e0f46a03b9584",
+    "url": "/multiplied-pairs/static/media/background.f01760eb.png"
+  }
+]);
